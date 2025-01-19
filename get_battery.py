6ea98dbc030631919e429fdb05b05c6ce15c2ec6
Dawid Karpiński <dawid.karp@int.pl>
2025-01-19 14:18:01 +0100

    feat: python script

---
+++ get_battery.py
@@ -0,0 +1,21 @@
+import time
+
+import serial
+
+port = "/dev/ttyACM0"
+baud_rate = 9600
+
+ser = serial.Serial(port, baud_rate, timeout=1)
+print(int(time.time() * 1000), end=" ")
+
+
+def read(ser):
+    return ser.read_until(".".encode("utf-8"))[:-1].strip()
+
+
+for side in ["left", "right"]:
+    ser.write(f"wireless.battery.{side}.level\n".encode("utf-8"))
+    print(int(read(ser)), end=" ")
+
+ser.write("wireless.battery.left.status\n".encode("utf-8"))
+print(int(read(ser)))
